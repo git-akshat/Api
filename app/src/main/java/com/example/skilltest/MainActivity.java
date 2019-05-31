96149a1db49887de16dc4a610fb7a908043372be
Akshat <mr.akshatagarwal26@gmail.com>
2019-05-31 17:28:05 +0530

    added Product class and created a static layout

--- app/src/main/java/com/example/skilltest/MainActivity.java
+++ app/src/main/java/com/example/skilltest/MainActivity.java
@@ -2,12 +2,134 @@ package com.example.skilltest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    TextView pname;
+    TextView pdesc;
+    ImageView pimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pname = (TextView) findViewById(R.id.product_name);
+        pdesc = (TextView) findViewById(R.id.product_description);
+        pimage = (ImageView) findViewById(R.id.image_view);
+        try {
+            parseXml();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseXml() throws XmlPullParserException, IOException {
+        XmlPullParserFactory parserFactory;
+        parserFactory = XmlPullParserFactory.newInstance();
+        XmlPullParser parser = parserFactory.newPullParser();
+        InputStream istream = getAssets().open("http://anecdot.in/api/product.xml");
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,false);
+        parser.setInput(istream,null);
+
+        processParsing(parser);
+    }
+
+    private void processParsing(XmlPullParser parser) throws XmlPullParserException, IOException {
+        ArrayList<Product> products = new ArrayList<>();
+        int eventType = parser.getEventType();
+        Product currentProduct = null;
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            String eltName = null;
+
+            switch (eventType) {
+                case XmlPullParser.START_TAG:
+                    eltName = parser.getName();
+
+                    if ("URL".equals(eltName))
+                    {
+                        currentProduct = new Product();
+                        products.add(currentProduct);
+                    }
+                    else if (currentProduct != null)
+                    {
+                        if ("product_id".equals(eltName)) {
+                            currentProduct.product_id = parser.nextText();
+                        }
+                        else if ("product_url".equals(eltName)) {
+                            currentProduct.product_url = parser.nextText();
+                        }
+                        else if ("product_name".equals(eltName)) {
+                            currentProduct.product_name = parser.nextText();
+                        }
+                        else if ("about_product".equals(eltName)) {
+                            currentProduct.about_product = parser.nextText();
+                        }
+                        else if ("product_image1".equals(eltName)) {
+                            currentProduct.product_image1 = parser.nextText();
+                        }
+                        else if ("product_image2".equals(eltName)) {
+                            currentProduct.product_image2 = parser.nextText();
+                        }
+                        else if ("product_image3".equals(eltName)) {
+                            currentProduct.product_image3 = parser.nextText();
+                        }
+                        else if ("product_image4".equals(eltName)) {
+                            currentProduct.product_image4 = parser.nextText();
+                        }
+                        else if ("product_image5".equals(eltName)) {
+                            currentProduct.product_image5 = parser.nextText();
+                        }
+                        else if ("price".equals(eltName)) {
+                            currentProduct.price = parser.nextText();
+                        }
+                        else if ("coupon_code".equals(eltName)) {
+                            currentProduct.coupon_code = parser.nextText();
+                        }
+                        else if ("product_desc".equals(eltName)) {
+                            currentProduct.product_desc = parser.nextText();
+                        }
+                        else if ("color".equals(eltName)) {
+                            currentProduct.color = parser.nextText();
+                        }
+                        else if ("RFID".equals(eltName)) {
+                            currentProduct.RFID = parser.nextText();
+                        }
+                        else if ("size_for_trail".equals(eltName)) {
+                            currentProduct.size_for_trail = parser.nextText();
+                        }
+                    }
+                    break;
+            }
+
+            eventType = parser.next();
+        }
+
+        printProducts(products);
+    }
+
+    private void printProducts(ArrayList<Product> products) {
+        StringBuilder builder = new StringBuilder();
+
+        for (Product product : products) {
+            builder.append(product.product_id).append("\n").
+                    append(product.product_url).append("\n").
+                    append(product.product_name).append("\n\n");
+        }
+
+        pname.setText(builder.toString());
     }
 }
+
---
+++ app/src/main/java/com/example/skilltest/Product.java
@@ -0,0 +1,140 @@
+package com.example.skilltest;
+
+public class Product {
+    public String product_id;
+    public String product_url;
+    public String product_name;
+    public String about_product;
+    public String product_image1;
+    public String product_image2;
+    public String product_image3;
+    public String product_image4;
+    public String product_image5;
+    public String price;
+    public String coupon_code;
+    public String product_desc;
+    public String color;
+    public String RFID;
+    public String size_for_trail;
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_url() {
+        return product_url;
+    }
+
+    public void setProduct_url(String product_url) {
+        this.product_url = product_url;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getAbout_product() {
+        return about_product;
+    }
+
+    public void setAbout_product(String about_product) {
+        this.about_product = about_product;
+    }
+
+    public String getProduct_image1() {
+        return product_image1;
+    }
+
+    public void setProduct_image1(String product_image1) {
+        this.product_image1 = product_image1;
+    }
+
+    public String getProduct_image2() {
+        return product_image2;
+    }
+
+    public void setProduct_image2(String product_image2) {
+        this.product_image2 = product_image2;
+    }
+
+    public String getProduct_image3() {
+        return product_image3;
+    }
+
+    public void setProduct_image3(String product_image3) {
+        this.product_image3 = product_image3;
+    }
+
+    public String getProduct_image4() {
+        return product_image4;
+    }
+
+    public void setProduct_image4(String product_image4) {
+        this.product_image4 = product_image4;
+    }
+
+    public String getProduct_image5() {
+        return product_image5;
+    }
+
+    public void setProduct_image5(String product_image5) {
+        this.product_image5 = product_image5;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCoupon_code() {
+        return coupon_code;
+    }
+
+    public void setCoupon_code(String coupon_code) {
+        this.coupon_code = coupon_code;
+    }
+
+    public String getProduct_desc() {
+        return product_desc;
+    }
+
+    public void setProduct_desc(String product_desc) {
+        this.product_desc = product_desc;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRFID() {
+        return RFID;
+    }
+
+    public void setRFID(String RFID) {
+        this.RFID = RFID;
+    }
+
+    public String getSize_for_trail() {
+        return size_for_trail;
+    }
+
+    public void setSize_for_trail(String size_for_trail) {
+        this.size_for_trail = size_for_trail;
+    }
+
+}
